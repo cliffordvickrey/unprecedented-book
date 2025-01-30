7ef392ec673299ebb5bb3e9377d730e093bf1d6a
cliffordvickrey <clifforddavidvickrey@gmail.com>
2025-01-29 22:14:51 -0500

    in progress

--- src/Common/Entity/Profile/DonorProfileCycle2020.php
+++ src/Common/Entity/Profile/DonorProfileCycle2020.php
@@ -8,24 +8,72 @@ use CliffordVickrey\Book2024\Common\Enum\PartyType;
 
 class DonorProfileCycle2020 extends DonorProfileCycle
 {
+    #[RecipientAttribute(PartyType::democrat, ['C00696419', 'C00738831', 'C00431874'])]
+    public bool $presAmyKlobuchar = false;
+    #[RecipientAttribute(PartyType::democrat, ['C00659938', 'C00712497', 'C00719708', 'C00730937', 'C00725994', 'C00721050'])]
+    public bool $presAndrewYang = false;
+    #[RecipientAttribute(PartyType::democrat, ['C00696948', 'C00686477', 'C00406553', 'C00698233', 'C00581967'])]
+    public bool $presBernieSanders = false;
+    #[RecipientAttribute(PartyType::democrat, ['C00699090', 'C00571174', 'C00656439'])]
+    public bool $presBetoORourke = false;
+    #[RecipientAttribute(PartyType::democrat, ['C00706697', 'C00683664', 'C00712539'])]
+    public bool $presBillDeBlasio = false;
     #[RecipientAttribute(PartyType::democrat, ['C00695510', 'C00726208', 'C00693028', 'C00497131'])]
     public bool $presCoryBooker = false;
+    #[RecipientAttribute(PartyType::democrat, ['C00727156', 'C00730317', 'C00743252', 'C00686394'])]
+    public bool $presDevalPatrick = false;
     #[RecipientAttribute(PartyType::republican, ['C00618371', 'C00580100', 'C00618389', 'C00637512', 'C00756882', 'C00762591', 'C00544767', 'C00608489', 'C00532630', 'C00650168', 'C00540898', 'C00688069', 'C00692129', 'C00754168', 'C00618876', 'C00739151', 'C00727230', 'C00505792', 'C00692467', 'C00566174', 'C00756254', 'C00697862', 'C00634717', 'C00683896', 'C00625574', 'C00622159', 'C00748657', 'C00528893', 'C00753921', 'C00759811', 'C00753673', 'C00720227', 'C00688093', 'C00733899', 'C00750224', 'C00753251', 'C00580373', 'C00761569', 'C00682898', 'C00626689', 'C00614370', 'C00575373', 'C00596973'])]
     public bool $presDonaldTrump = false;
+    #[RecipientAttribute(PartyType::democrat, ['C00693234', 'C00739110', 'C00714436'])]
+    public bool $presElizabethWarren = false;
+    #[RecipientAttribute(PartyType::democrat, ['C00701698', 'C00566059'])]
+    public bool $presEricSwalwell = false;
+    #[RecipientAttribute(PartyType::other, ['C00708024'])]
+    public bool $presHowieHawkins = false;
+    #[RecipientAttribute(PartyType::democrat, ['C00698050', 'C00697300', 'C00688937'])]
+    public bool $presJayInslee = false;
     #[RecipientAttribute(PartyType::democrat, ['C00703975', 'C00669259', 'C00495861', 'C00492140', 'C00532705', 'C00701888', 'C00672394', 'C00646877', 'C00636027', 'C00751420', 'C00744185', 'C00564013', 'C00752691', 'C00760678', 'C00748798', 'C00736637', 'C00748301', 'C00760116', 'C00531624', 'C00624056', 'C00755942', 'C00760488', 'C00639500', 'C00756973', 'C00750497', 'C00759159', 'C00742775', 'C00760728', 'C00688200', 'C00753293', 'C00755132', 'C00743021', 'C00748160', 'C00759878', 'C00721779', 'C00743625', 'C00757963', 'C00757187', 'C00752089', 'C00746099', 'C00679589', 'C00742932', 'C00758482', 'C00740175', 'C00757997', 'C00757542', 'C00758672'])]
     public bool $presJoeBiden = false;
+    #[RecipientAttribute(PartyType::democrat, ['C00710574', 'C00455741'])]
+    public bool $presJoeSestak = false;
+    #[RecipientAttribute(PartyType::republican, ['C00717033'])]
+    public bool $preJoeWalsh = false;
     #[RecipientAttribute(PartyType::democrat, ['C00508416', 'C00683136'])]
     public bool $presJohnDelaney = false;
+    #[RecipientAttribute(PartyType::democrat, ['C00698258', 'C00748756', 'C00687582', 'C00701128'])]
+    public bool $johnHickenlooper = false;
     #[RecipientAttribute(PartyType::other, ['C00718031'])]
     public bool $presJoJorgensen = false;
     #[RecipientAttribute(PartyType::democrat, ['C00693044', 'C00652552'])]
     public bool $presJulianCastro = false;
+    #[RecipientAttribute(PartyType::democrat, ['C00694455', 'C00713099', 'C00629071'])]
+    public bool $presKamalaHarris = false;
+    #[RecipientAttribute(PartyType::democrat, ['C00694018', 'C00525600', 'C00477067'])]
+    public bool $presKirstenGillibrand = false;
+    #[RecipientAttribute(PartyType::democrat, ['C00696054'])]
+    public bool $presMarianneWilliamson = false;
+    #[RecipientAttribute(PartyType::republican, ['C00285254', 'C00579516'])]
+    public bool $presMarkSanford = false;
     #[RecipientAttribute(PartyType::democrat, ['C00705186', 'C00491936'])]
-    public bool $presMichaelBennet  = false;
+    public bool $presMichaelBennet = false;
     #[RecipientAttribute(PartyType::democrat, ['C00728154'])]
     public bool $presMichaelBloomberg = false;
+    #[RecipientAttribute(PartyType::democrat, ['C00700609'])]
+    public bool $presMikeGravel = false;
     #[RecipientAttribute(PartyType::democrat, ['C00697441', 'C00648501'])]
     public bool $presPeteButtigieg = false;
+    #[RecipientAttribute(PartyType::democrat, ['C00704510', 'C00571174', 'C00656439'])]
+    public bool $presSethMoulton = false;
     #[RecipientAttribute(PartyType::democrat, ['C00706416', 'C00493262', 'C00744839', 'C00650754', 'C00760959'])]
     public bool $presSteveBullock = false;
+    #[RecipientAttribute(PartyType::democrat, ['C00701979', 'C00417584'])]
+    public bool $presTimRyan = false;
+    #[RecipientAttribute(PartyType::democrat, ['C00711614'])]
+    public bool $presTomSteyer = false;
+    #[RecipientAttribute(PartyType::democrat, ['C00693713'])]
+    public bool $presTulsiGabbard = false;
+    #[RecipientAttribute(PartyType::democrat, ['C00699280'])]
+    public bool $presWayneMessam = false;
+    #[RecipientAttribute(PartyType::republican, ['C00700906'])]
+    public bool $presWilliamWeld = false;
 }
