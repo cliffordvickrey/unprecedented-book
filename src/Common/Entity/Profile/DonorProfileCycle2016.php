43200cc94a75e64b21341da767448b1c98525a90
cliffordvickrey <clifforddavidvickrey@gmail.com>
2025-01-31 00:36:00 -0500

    in progress

--- src/Common/Entity/Profile/DonorProfileCycle2016.php
+++ src/Common/Entity/Profile/DonorProfileCycle2016.php
@@ -8,38 +8,171 @@ use CliffordVickrey\Book2024\Common\Enum\PartyType;
 
 class DonorProfileCycle2016 extends DonorProfileCycle
 {
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00573519', 'C00569905', 'C00548420', 'C00577296', 'C00586685', 'C00574277'])]
+    #[RecipientAttribute(
+        slug: 'ben_carson',
+        party: PartyType::republican,
+        startDate: '2015-05-03',
+        endDate: '2016-03-04',
+        committeeIds: ['C00573519', 'C00569905', 'C00548420', 'C00577296', 'C00586685', 'C00574277']
+    )]
     public bool $presBenCarson = false;
-    #[RecipientAttribute(party: PartyType::democrat, committeeIds: ['C00577130', 'C00492595', 'C00600817', 'C00406553', 'C00590646', 'C00590620', 'C00555615', 'C00589226', 'C00609602', 'C00589937', 'C00590240'])]
+    #[RecipientAttribute(
+        slug: 'bernie_sanders',
+        party: PartyType::democrat,
+        startDate: '2015-04-30',
+        endDate: '2016-07-12',
+        committeeIds: [
+            'C00577130',
+            'C00492595',
+            'C00600817',
+            'C00406553',
+            'C00590646',
+            'C00590620',
+            'C00555615',
+            'C00589226',
+            'C00609602',
+            'C00589937',
+            'C00590240'
+        ]
+    )]
     public bool $presBernieSanders = false;
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00580159', 'C00571711', 'C00559237'])]
+    #[RecipientAttribute(
+        slug: 'bobby_jindal',
+        party: PartyType::republican,
+        startDate: '2015-06-24',
+        endDate: '2015-11-17',
+        committeeIds: ['C00580159', 'C00571711', 'C00559237']
+    )]
     public bool $presBobbyJindal = false;
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00577312', 'C00573154', 'C00564534'])]
+    #[RecipientAttribute(
+        slug: 'carly_fiorina',
+        party: PartyType::republican,
+        startDate: '2015-05-04',
+        endDate: '2016-02-10',
+        committeeIds: ['C00577312', 'C00573154', 'C00564534']
+    )]
     public bool $presCarlyFiorina = false;
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00574251', 'C00573055', 'C00571778'])]
+    #[RecipientAttribute(
+        slug: 'chris_christie',
+        party: PartyType::republican,
+        startDate: '2015-06-30',
+        endDate: '2016-02-10',
+        committeeIds: ['C00574251', 'C00573055', 'C00571778']
+    )]
     public bool $presChrisChristie = false;
-    #[RecipientAttribute(party: PartyType::other, committeeIds: ['C00623884'])]
+    #[RecipientAttribute(
+        slug: 'evan_mcmullin',
+        party: PartyType::other,
+        startDate: '2016-08-08',
+        committeeIds: ['C00623884']
+    )]
     public bool $presEvanMcMullin = false;
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00578245', 'C00571356'])]
+    #[RecipientAttribute(
+        slug: 'george_pataki',
+        party: PartyType::republican,
+        startDate: '2015-05-28',
+        endDate: '2015-12-29',
+        committeeIds: ['C00578245', 'C00571356']
+    )]
     public bool $presGeorgePataki = false;
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00458844', 'C00541292', 'C00500025', 'C00591214'])]
-    public bool $presMarcoRubio = false;
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00618371', 'C00580100', 'C00618389', 'C00608489', 'C00574533', 'C00618876', 'C00575373', 'C00580373', 'C00589879', 'C00614370', 'C00612903', 'C00586826', 'C00616789', 'C00628396', 'C00591610', 'C00591610', 'C00607283', 'C00587881'])]
+    #[RecipientAttribute(
+        slug: 'donald_trump',
+        party: PartyType::republican,
+        startDate: '2015-06-16',
+        committeeIds: [
+            'C00618371',
+            'C00580100',
+            'C00618389',
+            'C00608489',
+            'C00574533',
+            'C00618876',
+            'C00575373',
+            'C00580373',
+            'C00589879',
+            'C00614370',
+            'C00612903',
+            'C00586826',
+            'C00616789',
+            'C00628396',
+            'C00591610',
+            'C00591610',
+            'C00607283',
+            'C00587881'
+        ]
+    )]
     public bool $presDonaldTrump = false;
-    #[RecipientAttribute(party: PartyType::democrat, committeeIds: ['C00575795', 'C00586537', 'C00495861', 'C00578997', 'C00540997', 'C00559765', 'C00570549', 'C00605204', 'C00566034', 'C00573741'])]
+    #[RecipientAttribute(
+        slug: 'hillary_clinton',
+        party: PartyType::democrat,
+        startDate: '2015-04-12',
+        committeeIds: [
+            'C00575795',
+            'C00586537',
+            'C00495861',
+            'C00578997',
+            'C00540997',
+            'C00559765',
+            'C00570549',
+            'C00605204',
+            'C00566034',
+            'C00573741'
+        ])]
     public bool $presHillaryClinton = false;
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00579458', 'C00571372', 'C00571950'])]
+    #[RecipientAttribute(
+        slug: 'jeb_bush',
+        party: PartyType::republican,
+        startDate: '2015-06-14',
+        endDate: '2016-02-20',
+        committeeIds: ['C00579458', 'C00571372', 'C00571950']
+    )]
     public bool $presJebBush = false;
-    #[RecipientAttribute(party: PartyType::democrat, committeeIds: ['C00581215', 'C00430819'])]
+    #[RecipientAttribute(
+        slug: 'jim_webb',
+        party: PartyType::democrat,
+        startDate: '2015-07-02',
+        endDate: '2015-10-20',
+        committeeIds: ['C00581215', 'C00430819']
+    )]
     public bool $presJimWebb = false;
-    #[RecipientAttribute(party: PartyType::other, committeeIds: ['C00581199'])]
+    #[RecipientAttribute(
+        slug: 'jill_stein',
+        party: PartyType::other,
+        startDate: '2015-06-22',
+        committeeIds: ['C00581199']
+    )]
     public bool $presJillStein = false;
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00582668', 'C00568840'])]
+    #[RecipientAttribute(
+        slug: 'jim_gilmore',
+        party: PartyType::republican,
+        startDate: '2015-07-29',
+        endDate: '2016-02-12',
+        committeeIds: ['C00582668', 'C00568840']
+    )]
     public bool $presJimGilmore = false;
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00581876', 'C00581868', 'C00582973'])]
+    #[RecipientAttribute(
+        slug: 'john_kasich',
+        party: PartyType::republican,
+        startDate: '2015-07-21',
+        endDate: '2016-05-04',
+        committeeIds: ['C00581876', 'C00581868', 'C00582973']
+    )]
     public bool $presJohnKasich = false;
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00578757', 'C00573733', 'C00388934', 'C00543157'])]
+    #[RecipientAttribute(
+        slug: 'lindsey_graham',
+        party: PartyType::republican,
+        startDate: '2015-07-21',
+        endDate: '2016-05-04',
+        committeeIds: ['C00578757', 'C00573733', 'C00388934', 'C00543157']
+    )]
     public bool $presLindseyGraham = false;
+    #[RecipientAttribute(
+        slug: 'marco_rubio',
+        party: PartyType::republican,
+        startDate: '2015-06-01',
+        endDate: '2015-12-21',
+        committeeIds: ['C00458844', 'C00541292', 'C00500025', 'C00591214']
+    )]
+    public bool $presMarcoRubio = false;
     #[RecipientAttribute(party: PartyType::democrat, committeeIds: ['C00578658', 'C00578724', 'C00525220'])]
     public bool $presMartinOMalley = false;
     #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00577981', 'C00573923', 'C00448373', 'C00579243'])]
--- src/Common/Entity/Profile/DonorProfileCycle2024.php
+++ src/Common/Entity/Profile/DonorProfileCycle2024.php
@@ -8,42 +8,340 @@ use CliffordVickrey\Book2024\Common\Enum\PartyType;
 
 class DonorProfileCycle2024 extends DonorProfileCycle
 {
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00798173', 'C00837104'])]
+    #[RecipientAttribute(
+        slug: 'asa_hutchinson',
+        party: PartyType::republican,
+        startDate: '2023-04-26',
+        endDate: '2024-01-16',
+        committeeIds: ['C00798173', 'C00837104']
+    )]
     public bool $presAsaHutchinson = false;
-    #[RecipientAttribute(party: PartyType::other, committeeIds: ['C00837625'])]
+    #[RecipientAttribute(
+        slug: 'chase_oliver',
+        party: PartyType::other,
+        startDate: '2023-04-04',
+        committeeIds: ['C00837625']
+    )]
     public bool $presChaseOliver = false;
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00841593', 'C00571778', 'C00842237'])]
+    #[RecipientAttribute(
+        slug: 'chris_christie',
+        party: PartyType::republican,
+        startDate: '2023-06-06',
+        endDate: '2024-01-10',
+        committeeIds: ['C00841593', 'C00571778', 'C00842237']
+    )]
     public bool $presChrisChristie = false;
-    #[RecipientAttribute(party: PartyType::other, committeeIds: ['C00843508'])]
+    #[RecipientAttribute(
+        slug: 'cornel_west',
+        party: PartyType::other,
+        startDate: '2023-06-05',
+        committeeIds: ['C00843508']
+    )]
     public bool $presCornelWest = false;
-    #[RecipientAttribute(party: PartyType::democrat, committeeIds: ['C00854778', 'C00858779', 'C00857383'])]
+    #[RecipientAttribute(
+        slug: 'dean_phillips',
+        party: PartyType::democrat,
+        startDate: '2023-10-27',
+        endDate: '2024-03-06',
+        committeeIds: ['C00854778', 'C00858779', 'C00857383']
+    )]
     public bool $presDeanPhillips = false;
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00828541', 'C00867937', 'C00873893', 'C00770941', 'C00580100', 'C00618371', 'C00855114', 'C00825851', 'C00879510', 'C00878801', 'C00762591', 'C00867036', 'C00883520', 'C00881987', 'C00891291', 'C00580100', 'C00881805', 'C00888172', 'C00540898', 'C00889931', 'C00494021', 'C00634261', 'C00771477', 'C00887703', 'C00637512', 'C00544767', 'C00750224', 'C00887729', 'C00692467', 'C00880815', 'C00886515', 'C00886317', 'C00883561', 'C00885855', 'C00608489', 'C00887745', 'C00881631', 'C00875963', 'C00885665', 'C00621755', 'C00790477'])]
+    #[RecipientAttribute(
+        slug: 'donald_trump',
+        party: PartyType::republican,
+        startDate: '2022-11-15',
+        committeeIds: [
+            'C00828541',
+            'C00867937',
+            'C00873893',
+            'C00770941',
+            'C00580100',
+            'C00618371',
+            'C00855114',
+            'C00825851',
+            'C00879510',
+            'C00878801',
+            'C00762591',
+            'C00867036',
+            'C00883520',
+            'C00881987',
+            'C00891291',
+            'C00580100',
+            'C00881805',
+            'C00888172',
+            'C00540898',
+            'C00889931',
+            'C00494021',
+            'C00634261',
+            'C00771477',
+            'C00887703',
+            'C00637512',
+            'C00544767',
+            'C00750224',
+            'C00887729',
+            'C00692467',
+            'C00880815',
+            'C00886515',
+            'C00886317',
+            'C00883561',
+            'C00885855',
+            'C00608489',
+            'C00887745',
+            'C00881631',
+            'C00875963',
+            'C00885665',
+            'C00621755',
+            'C00790477',
+        ]
+    )]
     public bool $presDonaldTrump = false;
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00842344', 'C00842302'])]
+    #[RecipientAttribute(
+        slug: 'doug_burgum',
+        party: PartyType::republican,
+        startDate: '2023-06-07',
+        endDate: '2023-12-04',
+        committeeIds: ['C00842344', 'C00842302']
+    )]
     public bool $presDougBurgum = false;
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00842971', 'C00801803'])]
+    #[RecipientAttribute(
+        slug: 'francis_suarez',
+        party: PartyType::republican,
+        startDate: '2023-06-14',
+        endDate: '2023-08-29',
+        committeeIds: ['C00842971', 'C00801803']
+    )]
     public bool $presFrancisSuarez = false;
-    #[RecipientAttribute(party: PartyType::other, committeeIds: ['C00856112'])]
+    #[RecipientAttribute(
+        slug: 'jill_stein',
+        party: PartyType::other,
+        startDate: '2023-11-09',
+        committeeIds: ['C00856112']
+    )]
     public bool $presJillStein = false;
-    #[RecipientAttribute(party: PartyType::democrat, committeeIds: ['C00703975', 'C00744946', 'C00838912', 'C00857177', 'C00845776', 'C00778381'])]
+    #[RecipientAttribute(
+        slug: 'joe_biden',
+        party: PartyType::democrat,
+        startDate: '2023-04-25',
+        endDate: '2024-07-20',
+        committeeIds: [
+            'C00703975',
+            'C00744946',
+            'C00838912',
+            'C00857177',
+            'C00845776',
+            'C00778381',
+            'C00669259',
+            'C00492140',
+            'C00631549',
+            'C00725820',
+            'C00753558',
+            'C00495861',
+            'C00885947',
+            'C00752048',
+            'C00547349',
+            'C00827253',
+            'C00701888',
+            'C00883827',
+            'C00875815',
+            'C00819631',
+            'C00844282',
+            'C00874115',
+            'C00882381',
+            'C00887182',
+            'C00718353',
+            'C00885137',
+            'C00812495',
+            'C00886093',
+            'C00695528',
+            'C00887968',
+            'C00879973',
+            'C00882233',
+            'C00853713',
+            'C00825737',
+            'C00890392',
+            'C00887414',
+            'C00891259',
+            'C00874750',
+            'C00531624',
+            'C00842575',
+            'C00800995',
+            'C00884239',
+            'C00703082',
+            'C00736637',
+            'C00739557',
+            'C00891325',
+            'C00882019',
+            'C00884817',
+            'C00877027',
+            'C00887992',
+            'C00752089',
+            'C00827030',
+            'C00889279',
+            'C00877886',
+            'C00891937',
+            'C00876854',
+            'C00891044',
+            'C00875443',
+            'C00528448',
+            'C00891028',
+            'C00887208',
+            'C00891382',
+            'C00889907',
+            'C00881573',
+        ]
+    )]
     public bool $presJoeBiden = false;
-    #[RecipientAttribute(party: PartyType::democrat, committeeIds: ['C00703975', 'C00744946', 'C00838912', 'C00669259', 'C00492140', 'C00631549', 'C00725820', 'C00753558', 'C00495861', 'C00885947', 'C00752048', 'C00547349', 'C00827253', 'C00701888', 'C00883827', 'C00875815', 'C00819631', 'C00844282', 'C00874115', 'C00882381', 'C00887182', 'C00718353', 'C00885137', 'C00812495', 'C00886093', 'C00695528', 'C00887968', 'C00879973', 'C00882233', 'C00853713', 'C00825737', 'C00890392', 'C00887414', 'C00891259', 'C00874750', 'C00531624', 'C00842575', 'C00800995', 'C00884239', 'C00703082', 'C00736637', 'C00739557', 'C00891325', 'C00882019', 'C00884817', 'C00877027', 'C00887992', 'C00752089', 'C00827030', 'C00889279', 'C00877886', 'C00891937', 'C00876854', 'C00891044', 'C00875443', 'C00528448', 'C00891028', 'C00887208', 'C00891382', 'C00889907', 'C00881573'])]
+    #[RecipientAttribute(
+        slug: 'kamala_harris',
+        party: PartyType::democrat,
+        startDate: '2023-04-25',
+        endDate: '2024-07-20',
+        committeeIds: [
+            'C00703975',
+            'C00744946',
+            'C00838912',
+            'C00669259',
+            'C00492140',
+            'C00631549',
+            'C00725820',
+            'C00753558',
+            'C00495861',
+            'C00885947',
+            'C00752048',
+            'C00547349',
+            'C00827253',
+            'C00701888',
+            'C00883827',
+            'C00875815',
+            'C00819631',
+            'C00844282',
+            'C00874115',
+            'C00882381',
+            'C00887182',
+            'C00718353',
+            'C00885137',
+            'C00812495',
+            'C00886093',
+            'C00695528',
+            'C00887968',
+            'C00879973',
+            'C00882233',
+            'C00853713',
+            'C00825737',
+            'C00890392',
+            'C00887414',
+            'C00891259',
+            'C00874750',
+            'C00531624',
+            'C00842575',
+            'C00800995',
+            'C00884239',
+            'C00703082',
+            'C00736637',
+            'C00739557',
+            'C00891325',
+            'C00882019',
+            'C00884817',
+            'C00877027',
+            'C00887992',
+            'C00752089',
+            'C00827030',
+            'C00889279',
+            'C00877886',
+            'C00891937',
+            'C00876854',
+            'C00891044',
+            'C00875443',
+            'C00528448',
+            'C00891028',
+            'C00887208',
+            'C00891382',
+            'C00889907',
+            'C00881573',
+        ]
+    )]
     public bool $presKamalaHarris = false;
-    #[RecipientAttribute(party: PartyType::democrat, committeeIds: ['C00834424', 'C00867770'])]
+    #[RecipientAttribute(
+        slug: 'marianne_williamson',
+        party: PartyType::democrat,
+        startDate: '2023-03-04',
+        endDate: '2024-07-29',
+        committeeIds: ['C00834424', 'C00867770']
+    )]
     public bool $presMarianneWilliamson = false;
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00842039', 'C00839464', 'C00640664'])]
+    #[RecipientAttribute(
+        slug: 'mike_pence',
+        party: PartyType::republican,
+        startDate: '2023-06-05',
+        endDate: '2024-10-28',
+        committeeIds: ['C00842039', 'C00839464', 'C00640664']
+    )]
     public bool $presMikePence = false;
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00833392', 'C00828061', 'C00765982', 'C00858381'])]
+    #[RecipientAttribute(
+        slug: 'nikki_haley',
+        party: PartyType::republican,
+        startDate: '2023-02-14',
+        endDate: '2024-03-06',
+        committeeIds: ['C00833392', 'C00828061', 'C00765982', 'C00858381']
+    )]
     public bool $presNikkiHaley = false;
-    #[RecipientAttribute(party: PartyType::other, committeeIds: ['C00836916', 'C00821439', 'C00838805', 'C00840660', 'C00843938', 'C00851451', 'C00858670', 'C00870410'])]
+    #[RecipientAttribute(
+        slug: 'robert_f_kennedy_jr',
+        party: PartyType::other,
+        startDate: '2023-04-19',
+        endDate: '2024-08-23',
+        committeeIds: [
+            'C00836916',
+            'C00821439',
+            'C00838805',
+            'C00840660',
+            'C00843938',
+            'C00851451',
+            'C00858670',
+            'C00870410',
+        ]
+    )]
     public bool $presRfkJr = false;
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00834077', 'C00834853', 'C00857011', 'C00841148', 'C00815928', 'C00857425', 'C00836395', 'C00841734', 'C00828400'])]
+    #[RecipientAttribute(
+        slug: 'ron_desantis',
+        party: PartyType::republican,
+        startDate: '2023-05-24',
+        endDate: '2024-01-21',
+        committeeIds: [
+            'C00834077',
+            'C00834853',
+            'C00857011',
+            'C00841148',
+            'C00815928',
+            'C00857425',
+            'C00836395',
+            'C00841734',
+            'C00828400',
+        ]
+    )]
     public bool $presRonDeSantis = false;
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00540302', 'C00840546', 'C00825158', 'C00827519', 'C00750182'])]
+    #[RecipientAttribute(
+        slug: 'tim_scott',
+        party: PartyType::republican,
+        startDate: '2023-05-22',
+        endDate: '2023-11-12',
+        committeeIds: ['C00540302', 'C00840546', 'C00825158', 'C00827519', 'C00750182']
+    )]
     public bool $presTimScott = false;
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00833913', 'C00833749'])]
+    #[RecipientAttribute(
+        slug: 'vivek_ramaswamy',
+        party: PartyType::republican,
+        startDate: '2023-02-21',
+        endDate: '2024-01-15',
+        committeeIds: ['C00833913', 'C00833749']
+    )]
     public bool $presVivekRamaswamy = false;
-    #[RecipientAttribute(party: PartyType::republican, committeeIds: ['C00843540', 'C00693531'])]
+    #[RecipientAttribute(
+        slug: 'will_hurd',
+        party: PartyType::republican,
+        startDate: '2023-06-22',
+        endDate: '2023-10-09',
+        committeeIds: ['C00843540', 'C00693531']
+    )]
     public bool $presWillHurd = false;
 }
