ab0e220711bd1572e5b8b4448a00ca290ecddcd2
cliffordvickrey <clifforddavidvickrey@gmail.com>
2025-02-11 08:32:03 -0500

    js fiasco

--- js/src/utils.ts
+++ js/src/utils.ts
@@ -5,21 +5,47 @@ function formToObject(form: HTMLFormElement): any {
   formData.forEach((value, key) => {
     const strValue = value as string;
 
-    if (object.hasOwnProperty(key)) {
-      if (!Array.isArray(object[key])) {
-        object[key] = [object[key]];
+    let prop = key;
+
+    if (prop.endsWith("[]")) {
+      prop = prop.slice(0, -2);
+
+      if (!object.hasOwnProperty(prop)) {
+        object[prop] = [];
       }
-      object[key].push(strValue);
+    }
+
+    if (object.hasOwnProperty(prop)) {
+      if (!Array.isArray(object[prop])) {
+        object[prop] = [object[prop]];
+      }
+      object[prop].push(strValue);
     } else {
-      object[key] = strValue;
+      object[prop] = strValue;
     }
   });
+
+  console.log(object);
 
   return object;
 }
 
-function queryString(object: any) {
-  return new URLSearchParams(object).toString();
+function queryString(object: any): string {
+  const params: string[] = [];
+
+  for (const [key, value] of Object.entries(object)) {
+    const strValue = value as string;
+
+    if (Array.isArray(value)) {
+      value.forEach((v) => {
+        params.push(`${encodeURIComponent(key)}[]=${encodeURIComponent(v)}`);
+      });
+    } else {
+      params.push(`${encodeURIComponent(key)}=${encodeURIComponent(strValue)}`);
+    }
+  }
+
+  return params.join("&");
 }
 
 export function formToUrl(form: HTMLFormElement, extras: object = {}): any {
